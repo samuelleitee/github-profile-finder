c9371ac1ec552bbf617e90fae4b6704db3efef29
samuelleitee <programmer.and.investor@gmail.com>
2024-06-13 22:21:32 -0300

    Github Profile Finder

---
+++ src/components/ProfileCard/index.jsx
@@ -0,0 +1,7 @@
+import { ProfileCardRoot } from "./ProfileCardRoot";
+import { ProfileCardImage } from "./PofileCardImage";
+
+export const ProfileCard = {
+    Root: ProfileCardRoot,
+    Image: ProfileCardImage
+}
