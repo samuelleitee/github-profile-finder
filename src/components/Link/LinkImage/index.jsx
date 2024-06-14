8564a6ff68843fa3396195be818417d5745a54fc
samuelleitee <programmer.and.investor@gmail.com>
2024-06-13 22:21:19 -0300

    Github Profile Finder

---
+++ src/components/Link/LinkImage/index.jsx
@@ -0,0 +1,6 @@
+import LinkIcon from "../../../assets/ic_arrow-up-right-from-square-solid.svg";
+
+
+export function LinkImage() {
+  return <img src={LinkIcon} alt="Link" height={12} width={12}/>;
+}
