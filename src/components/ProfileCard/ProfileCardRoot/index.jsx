4c83d97f4a1b5b4c919699d1fce709eb59baf554
samuelleitee <programmer.and.investor@gmail.com>
2024-06-18 22:11:35 -0300

    Github Profile Finder

--- src/components/ProfileCard/ProfileCardRoot/index.jsx
+++ src/components/ProfileCard/ProfileCardRoot/index.jsx
@@ -1,12 +1,17 @@
 import styles from "./ProfileCardRoot.module.css";
 
-export function ProfileCardRoot({ children }) {
+export function ProfileCardRoot({ children, name }) {
   return (
     <div className={styles.container}>
-      <div>
-        <h2>Nome</h2>
-      </div>
       {children}
+      <div>
+        <h2 className={styles.name}>{name}</h2>
+        <p className={styles.description}>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </p>
+      </div>
     </div>
   );
 }
