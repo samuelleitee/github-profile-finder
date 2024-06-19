983530325bb7fbc92ced3d9167549dc3580b8efb
samuelleitee <programmer.and.investor@gmail.com>
2024-06-18 22:11:32 -0300

    Github Profile Finder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Link } from "./components/Link";
-import { LinkImage } from "./components/Link/LinkImage";
 import { ProfileCard } from "./components/ProfileCard";
 
 function App() {
   return (
-    <ProfileCard.Root>
+    <ProfileCard.Root name="Cameron Williamson">
       <ProfileCard.Image src="https://avatars.githubusercontent.com/u/131712358?v=4" />
 
       <Link.Root text="GITHUB">
-        <LinkImage />
+        <Link.Image />
       </Link.Root>
     </ProfileCard.Root>
   );
